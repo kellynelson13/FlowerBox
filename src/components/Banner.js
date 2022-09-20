7ff1514d053a68970b6ac99e0122046e34f9ab35
Kelly Nelson <kelnel86@gmail.com>
2022-09-20 08:55:36 -0500

    styles nav and banner

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+const Banner = () => {
+  return (
+    <div id="banner">
+      <div id="text">
+        <h1>flowerbox.</h1>
+        <h5>FOR ALL YOUR FLORAL NEEDS</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
